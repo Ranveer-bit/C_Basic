f0d48681014ba7626ba6d07000516f38dd3d53af
Ranveer-bit <choudharyranveer54@gmail.com>
2025-08-11 20:53:26 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1 = 2;
+    int num2 = 3;
+    int sum;
+
+    sum = num1 + num2;
+
+
+    printf("First number : %d\n" , num1);
+    printf("Second number : %d\n" , num2);
+    printf("Sum = %d\n" , sum);
+
+    return 0;
+    
+}
